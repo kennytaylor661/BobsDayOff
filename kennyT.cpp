01d962c891cdd0bea5ccc97d457d55deb8e78ed9
Kenny Taylor <kennytaylor@runbox.com>
2018-09-25 11:00:51 -0700

    Updated Kenny file

--- kennyT.cpp
+++ kennyT.cpp
@@ -1,9 +1,9 @@
 // Kenny's code goes here
 
-showKennyName(int x, int y)
-{
-    rect r;
-    r.bot = y;
-    r.left = x;
-    ggprint8b(&r, 16, 0x000000ff, "Kenny Taylor");
-}
+//showKennyName(int x, int y)
+//{
+//    rect r;
+//    r.bot = y;
+//    r.left = x;
+//    ggprint8b(&r, 16, 0x000000ff, "Kenny Taylor");
+//}
