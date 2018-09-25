f4d77b178969273378a43ee7708563ff836ed6a0
Tristan <littlepoindexter@gmail.com>
2018-09-25 11:40:22 -0700

    Added attributes to Enemies

--- Enemy.h
+++ Enemy.h
@@ -6,12 +6,15 @@ class Enemy
 {
 	int posx;
 	int posy;
+	int HP;
+	int damage;
+	int id;
 	Enemy(int x, int y): posx(x), posy(y){}
 
 public:
 	void moveLeft() {posx--;}
 	void moveRight() {posx++;}
-
+	bool findPlayer(player P) {return P.x > posx;}
 };
 
 #ENDIF
