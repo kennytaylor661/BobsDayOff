1d34cca3d859c75ffd3c2c20311d92daba28da4c
Kenny Taylor <kennytaylor@runbox.com>
2018-10-16 12:15:23 -0700

    Updated

--- bobsdayoff.cpp
+++ bobsdayoff.cpp
@@ -42,10 +42,12 @@ typedef Flt	Matrix[4][4];
 #define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
                       (c)[1]=(a)[1]-(b)[1]; \
                       (c)[2]=(a)[2]-(b)[2]
+
 //constants
 const float timeslice = 1.0f;
 const float gravity = -0.2f;
 #define ALPHA 1
+#define SHOW_FAKE_SHADOW
 
 //function prototypes
 void initOpengl();
