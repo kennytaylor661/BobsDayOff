bbc14c5a319d69cd54eb2576543ffca7d4f72c1d
Kenny Taylor <kennytaylor@runbox.com>
2018-11-18 14:59:54 -0800

    Minor code cleanup

--- bobsdayoff.cpp
+++ bobsdayoff.cpp
@@ -123,64 +123,6 @@ Global gl;
 // Start on level 1
 Level lev;
 
-/*
-Level::Level() {
-	load((char *)"level1.txt");
-}
-
-void Level::load(char *filename)
-{
-    //Log("Level constructor\n");
-    tilesize[0] = 32;
-    tilesize[1] = 32;
-    ftsz[0] = (Flt)tilesize[0];
-    ftsz[1] = (Flt)tilesize[1];
-    tile_base = 140.0;
-    //read level
-    //FILE *fpi = fopen("level1.txt","r");
-    FILE *fpi = fopen(filename, "r");
-    if (fpi) {
-        nrows=0;
-        char line[200];
-        while (fgets(line, 200, fpi) != NULL) {
-            // Ignore comments
-            if (line[0] != '#') {
-                removeCrLf(line);
-                int slen = strlen(line);
-                ncols = slen;
-                //Log("line: %s\n", line);
-                for (int j=0; j<slen; j++) {
-                    arr[nrows][j] = line[j];
-                }
-                ++nrows;
-            }
-        }
-        fclose(fpi);
-        //printf("nrows of background data: %i\n", nrows);
-    }    
-    for (int i=0; i<nrows; i++) {
-        for (int j=0; j<ncols; j++) {
-            printf("%c", arr[i][j]);
-        }
-        printf("\n");
-    }
-
-    // Set player initial location of (gl.xres/2, ?)
-
-}
-void Level::removeCrLf(char *str) {
-	//remove carriage return and linefeed from a Cstring
-	char *p = str;
-	while (*p) {
-		if (*p == 10 || *p == 13) {
-			*p = '\0';
-			break;
-		}
-		++p;
-	}
-}
-*/
-
 //X Windows variables
 class X11_wrapper {
 private:
